4368d32749a0c9a22b8ef86aba523c63dec50a36
tchantas <tchantasarn@gmail.com>
2026-09-05 18:40:45 +0700

    ft_strrchr & ft_strchr

---
+++ ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchantas <tchantasarn@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/05 18:18:32 by tchantas          #+#    #+#             */
+/*   Updated: 2026/09/05 18:40:17 by tchantas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	len;
+
+	len = ft_strlen(s);
+	while (len >= 0)
+	{
+		if (s[len] == (char)c)
+			return ((char *)&s[len]);
+		len--;
+	}
+	return (NULL);
+}
