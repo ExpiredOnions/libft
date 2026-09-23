3b34737141d2ad1e8e0dd8e54c789e8b44d39da1
tchantas <tchantasarn@gmail.com>
2026-09-23 18:32:00 +0700

    fix declaration

--- libft.h
+++ libft.h
@@ -69,7 +69,7 @@ size_t				ft_strlcat(char *dst, const char *src, size_t size);
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 void				ft_lstadd_back(t_list **lst, t_list *new);
-int					ft_lstsize(t_list *lst);
+unsigned int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
